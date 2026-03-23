ad93d540c56f1dfce92ee12b80e82c9d900be669
quantockhills <madhav.mohan@protonmail.com>
2026-03-23 10:49:07 +0100

    build: Add setup.py for pip installation
    - Enables pip install -e . for editable installs
    - Works from any directory once installed
    - Includes dependencies and extras for dev/analysis

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""Setup script for qneural package."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="qneural",
+    version="0.1.0",
+    author="Madhav Mohan, Julius de Hond",
+    author_email="madhav.mohan@protonmail.com",
+    description="Machine Learning for Quantum Control",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quantockhills/qneural",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "torch>=1.10",
+        "numpy",
+        "torchdiffeq",
+        "matplotlib",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+        ],
+        "analysis": [
+            "qutip",
+        ],
+    },
+)
